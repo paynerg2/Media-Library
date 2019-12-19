1bd1aa5ec2ac7fd3aef7f9a02218ef199e83d88a
Rodney Payne <paynerg2@gmail.com>
2019-12-19 16:46:04 -0500

    Remove console.logs

--- src/client/_hooks/useGame.ts
+++ src/client/_hooks/useGame.ts
@@ -3,8 +3,6 @@ import { useSelector } from '.';
 
 export const useGame = (id: string) => {
     const game: Game = useSelector(state => state.games.byId[id]);
-    console.log('game from usegame');
-    console.log(game);
     const publisher = useSelector(state =>
         state.companies.allIds
             .map(id => state.companies.byId[id])
