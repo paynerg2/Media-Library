47aea135ee83341682617ab23bb73495c7cfb098
Rodney Payne <paynerg2@gmail.com>
2020-01-04 02:29:47 -0500

    Add theme
    File containing all repeated style values.

--- src/client/App/theme.js
+++ src/client/App/theme.js
@@ -1,5 +1,17 @@
 export const theme = {
     itemHeight: '30vh',
     itemWidth: '20vh',
-    overlayHeight: '12vh'
+    overlayHeight: '12vh',
+
+    colors: {
+        primary: '#5da9e9',
+        secondary: '#003f91',
+        shadow: 'darkgrey',
+        text: 'black'
+    },
+
+    fonts: {
+        primary: 'Arial',
+        secondary: 'Helvetica'
+    }
 };
