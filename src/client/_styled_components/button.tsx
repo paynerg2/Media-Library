a58f453d720509094fee3f7e6bb20ee7c2b6e5cc
Rodney Payne <paynerg2@gmail.com>
2020-01-08 12:57:22 -0500

    Add basic styled button.
    Optional styling left to be added.

--- src/client/_styled_components/button.tsx
+++ src/client/_styled_components/button.tsx
@@ -11,6 +11,17 @@ export const Button = styled.button`
     font-family: ${props => props.theme.fonts.secondary};
     text-transform: uppercase;
     font-weight: bold;
-
+    box-shadow: 1px 2px 6px 1px darkgrey;
     margin-left: 2vw;
+    border: solid 2px darkslateblue;
+
+    &:hover {
+        transition: 0.1s ease-in;
+        transform: scale(1.02);
+    }
+
+    &:active:hover,
+    :active:focus {
+        box-shadow: none;
+    }
 `;
