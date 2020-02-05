190be663738e30b727ac2ad353a58da3f49d2d02
Rodney Payne <paynerg2@gmail.com>
2020-02-05 13:40:44 -0500

    (Refactor) Relocate buttons into Entity component

--- src/client/Views/DiscDisplayPage/DiscDisplayPage.tsx
+++ src/client/Views/DiscDisplayPage/DiscDisplayPage.tsx
@@ -1,13 +1,10 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { withRouter, RouteComponentProps } from 'react-router';
-import { useDispatch } from 'react-redux';
 import Disc from '../../_components/Disc/Disc';
-import { discActions } from '../../_actions';
 import { useSelector } from '../../_hooks';
 
 const DiscDisplayPage: React.FunctionComponent<DiscDisplayPageProps> = props => {
     const { id } = props.match.params;
-    const dispatch = useDispatch();
     const [loading, setLoading] = useState(true);
     const loadingStatus = useSelector(state => state.discs.loading);
 
@@ -15,25 +12,9 @@ const DiscDisplayPage: React.FunctionComponent<DiscDisplayPageProps> = props => 
         setLoading(loadingStatus);
     }, [loadingStatus]);
 
-    const handleEdit = () => {
-        props.history.push(`/discs/edit/${id}`);
-    };
-
-    const handleDelete = () => {
-        dispatch(discActions.delete(id));
-    };
-
     return (
         <Fragment>
-            {loading ? (
-                <div>Loading...</div>
-            ) : (
-                <Fragment>
-                    <Disc id={id} />
-                    <button onClick={handleEdit}>Edit</button>
-                    <button onClick={handleDelete}>Delete</button>
-                </Fragment>
-            )}
+            {loading ? <div>Loading...</div> : <Disc id={id} />}
         </Fragment>
     );
 };
--- src/client/Views/GameDisplayPage/GameDisplayPage.tsx
+++ src/client/Views/GameDisplayPage/GameDisplayPage.tsx
@@ -1,13 +1,10 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { withRouter, RouteComponentProps } from 'react-router';
 import Game from '../../_components/Game/Game';
-import { useDispatch } from 'react-redux';
 import { useSelector } from '../../_hooks';
-import { gameActions } from '../../_actions';
 
 const GameDisplayPage: React.FunctionComponent<GameDisplayPageProps> = props => {
     const { id } = props.match.params;
-    const dispatch = useDispatch();
     const [loading, setLoading] = useState(true);
     const loadingStatus = useSelector(state => state.games.loading);
 
@@ -15,28 +12,9 @@ const GameDisplayPage: React.FunctionComponent<GameDisplayPageProps> = props => 
         setLoading(loadingStatus);
     }, [loadingStatus]);
 
-    const handleEdit = () => {
-        props.history.push(`/games/edit/${id}`);
-    };
-
-    const handleDelete = () => {
-        dispatch(gameActions.delete(id));
-        //props.history.push('/');
-    };
-
-    console.log(loading);
-
     return (
         <Fragment>
-            {loading ? (
-                <div>Loading...</div>
-            ) : (
-                <Fragment>
-                    <Game id={id} />
-                    <button onClick={handleEdit}>Edit</button>
-                    <button onClick={handleDelete}>Delete</button>
-                </Fragment>
-            )}
+            {loading ? <div>Loading...</div> : <Game id={id} />}
         </Fragment>
     );
 };
