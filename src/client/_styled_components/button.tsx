d18075db2ceafbac6a7faa3183e3f39e5583a6dc
Rodney Payne <paynerg2@gmail.com>
2020-02-27 12:03:49 -0500

    (Refactor) Add color prop

--- src/client/_styled_components/button.tsx
+++ src/client/_styled_components/button.tsx
@@ -1,7 +1,22 @@
 import styled from 'styled-components';
 
-export const Button = styled.button`
-    background-color: ${props => props.theme.colors.secondary};
+export const colors = {
+    neutral: 'neutral',
+    danger: 'danger',
+    default: 'default'
+};
+
+export const Button = styled('button')<{ color?: string }>`
+    background-color: ${props => {
+        switch (props.color) {
+            case colors.neutral:
+                return 'lightGrey';
+            case colors.danger:
+                return props.theme.colors.error;
+            default:
+                return props.theme.colors.secondary;
+        }
+    }};
     border: none;
     height: 5vh;
     width: 20vh;
@@ -14,7 +29,17 @@ export const Button = styled.button`
     margin-bottom: 0.6vh;
     padding-left: 0.3vh;
     padding-right: 0.3vh;
-    border: solid 2px ${props => props.theme.colors.secondary};
+    border: solid 2px
+        ${props => {
+            switch (props.color) {
+                case colors.neutral:
+                    return 'lightGrey';
+                case colors.danger:
+                    return props.theme.colors.error;
+                default:
+                    return props.theme.colors.secondary;
+            }
+        }};
     cursor: pointer;
 
     & > {
