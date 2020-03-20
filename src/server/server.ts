3fabdb2dbb202f4b5df42a8eec7aa98dff3e3bb2
Rodney Payne <paynerg2@gmail.com>
2020-03-20 19:52:55 -0400

    Add https server for production

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,3 +1,5 @@
+import http from 'http';
+import https from 'https';
 import { connect } from './_helpers/connect';
 import { app } from './app';
 import { logger } from './_helpers/logger';
@@ -5,9 +7,14 @@ import { logger } from './_helpers/logger';
 // Start Server
 const PORT =
     process.env.NODE_ENV === 'production' ? process.env.PORT || 80 : 4000;
-app.listen(PORT, () => {
-    logger.info(`Server listening on port ${PORT}`);
-});
+
+process.env.NODE_ENV === 'production'
+    ? https.createServer(app).listen(PORT, () => {
+          logger.info(`Secure server listening on port ${PORT}`);
+      })
+    : http.createServer(app).listen(PORT, () => {
+          logger.info(`Server listening on port ${PORT}`);
+      });
 
 // Connect to database
 connect();
