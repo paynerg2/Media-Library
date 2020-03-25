feb53fe4820dccaa855059e6ee2347d7ea6152fb
Rodney Payne <paynerg2@gmail.com>
2020-03-25 12:32:50 -0400

    Remove https

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,4 @@
 import http from 'http';
-import https from 'https';
 import { connect } from './_helpers/connect';
 import { app } from './app';
 import { logger } from './_helpers/logger';
@@ -8,13 +7,9 @@ import { logger } from './_helpers/logger';
 const PORT =
     process.env.NODE_ENV === 'production' ? process.env.PORT || 80 : 4000;
 
-process.env.NODE_ENV === 'production'
-    ? https.createServer(app).listen(PORT, () => {
-          logger.info(`Secure server listening on port ${PORT}`);
-      })
-    : http.createServer(app).listen(PORT, () => {
-          logger.info(`Server listening on port ${PORT}`);
-      });
+http.createServer(app).listen(PORT, () => {
+    logger.info(`Server listening on port ${PORT}`);
+});
 
 // Connect to database
 connect();
