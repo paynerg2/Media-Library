4affc6972e72bb68ea2c3ca7ba19e342a56e9ab8
Rodney Payne <paynerg2@gmail.com>
2020-03-25 12:24:43 -0400

    Update for Heroku config variables

--- server/_helpers/env.ts
+++ server/_helpers/env.ts
@@ -6,7 +6,7 @@ config({ path: resolve(__dirname, '../../.env') });
 // Importing env variables from this module ensures that config
 // runs before they are expected.
 export const env = {
-    connectionString: process.env.CONNECTION_STRING,
-    secret: process.env.SECRET,
+    connectionString: process.env.CONNECTION_STRING || process.env.MONGODB_URI,
+    secret: process.env.SECRET || process.env.PROD_SECRET,
     testConnectionString: process.env.TEST_DB_CONNECTION_STRING
 };
